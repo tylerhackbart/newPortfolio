7e4541e2631dc7b803234cc4a1d73b56f1ead890
tylerhackbart <t.hackbart@gmail.com>
2015-12-21 21:12:46 -0500

    Added in graph.js

--- new-portfolio.js
+++ new-portfolio.js
@@ -55,6 +55,39 @@ $(".circle").mouseenter(function() {
 
 });
 
+  window.onload = function(){
+    window.myRadar = new Chart(document.getElementById("canvas").getContext("2d")).Radar(radarChartData, {
+      responsive: true
+    });
+  }
+
+
+var radarChartData = {
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+    datasets: [
+      {
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65,59,90,81,56,55,40]
+      },
+      {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [28,48,40,19,96,27,100]
+      }
+    ]
+  };
+
 function onScroll(event){
     var scrollPosition = $(document).scrollTop();
     $('#menu-list a').each(function () {
