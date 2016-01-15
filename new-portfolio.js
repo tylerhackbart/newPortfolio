492f3934d7e94023bb67450b08377b97bd07a2d5
tylerhackbart <t.hackbart@gmail.com>
2016-01-14 19:33:36 -0500

    Added in new email form

--- new-portfolio.js
+++ new-portfolio.js
@@ -115,6 +115,49 @@ function deleteEverything() {
   $('body').hide();
 }
 
+$(function() {
+    var form = $('#ajax-contact');
+    var formMessages = $('#form-messages');
+});
+
+$(form).submit(function(event) {
+    event.preventDefault();
+});
+
+var formData = $(form).serialize();
+
+$.ajax({
+    type: 'POST',
+    url: $(form).attr('action'),
+    data: formData
+})
+.done(function(response) {
+    // Make sure that the formMessages div has the 'success' class.
+    $(formMessages).removeClass('error');
+    $(formMessages).addClass('success');
+
+    // Set the message text.
+    $(formMessages).text(response);
+
+    // Clear the form.
+    $('#name').val('');
+    $('#email').val('');
+    $('#subject').val('');
+    $('#message').val('');
+})
+.fail(function(data) {
+    // Make sure that the formMessages div has the 'error' class.
+    $(formMessages).removeClass('success');
+    $(formMessages).addClass('error');
+
+    // Set the message text.
+    if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+    } else {
+        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    }
+});
+
 
 
 
